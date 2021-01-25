dff304b111e0bae6544f9327b80a7beedfbcefee
Maguas Tupaguas <mauricioaznar94@gmail.com>
2021-01-24 18:30:41 -0600

    61

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,10 @@ import Spinner from '../layout/Spinner'
 import {getProfileById} from '../../actions/profile'
 import {Link} from "react-router-dom";
 import ProfileTop from "./ProfileTop";
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 import ProfileAbout from "./ProfileAbout";
+import ProfileGithub from "./ProfileGithub";
 
 const Profile = (props) => {
   const {match, getProfileById, profile: {profile, loading}, auth} = props
@@ -25,12 +28,55 @@ const Profile = (props) => {
             </Link>
             {
               auth.isAuthenticated && !auth.loading && auth.user._id === profile.user._id
-              ? <Link to="/edit-profile" classNmae>Edit profile</Link>
+              ? <Link to="/edit-profile">Edit profile</Link>
               : null
             }
             <div className="profile-grid my-1">
               <ProfileTop profile={profile} />
               <ProfileAbout profile={profile} />
+              <div className="profile-exp bg-white p-2">
+                <h1 className="text-primary">
+                  Experience
+                </h1>
+                {
+                  profile.experience.length > 0
+                    ? <React.Fragment>
+                      {
+                        profile.experience.map(experience => {
+                          return <ProfileExperience
+                            key={experience._id}
+                            experience={experience}
+                          />
+                        })
+                      }
+                    </React.Fragment>
+                    : <h4>No experience credentials</h4>
+                }
+              </div>
+              <div className="profile-edu bg-white p-2">
+                <h1 className="text-primary">
+                  Education
+                </h1>
+                {
+                  profile.education.length > 0
+                    ? <React.Fragment>
+                      {
+                        profile.education.map(education => {
+                          return <ProfileEducation
+                            key={education._id}
+                            education={education}
+                          />
+                        })
+                      }
+                    </React.Fragment>
+                    : <h4>No experience credentials</h4>
+                }
+              </div>
+              {
+                profile.githubusername && (
+                  <ProfileGithub username={profile.githubusername} />
+                )
+              }
             </div>
           </React.Fragment>
       }
---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux'
+import {getGithubRepos} from "../../actions/profile";
+import Spinner from "../layout/Spinner";
+
+
+const ProfileGithub = ({username, getGithubRepos, repos}) => {
+
+  React.useEffect(() => {
+    getGithubRepos(username)
+  }, [getGithubRepos, username])
+
+  return (
+    <div className="profile-github">
+      <h2 className="text-primary my-1">Github repos</h2>
+      {
+        repos === null
+          ? <Spinner />
+          : repos.map(repo => {
+            return <div className="repo bg-white p-1 my-1" key={repo._id}>
+              <div>
+                <h4>
+                  <a href={repo.html_url} target="_blank" rel="nooponer noreferrer">
+                    {
+                      repo.name
+                    }
+                  </a>
+                </h4>
+                <p>{repo.description}</p>
+              </div>
+              <div>
+                <ul>
+                  <li className="badge badge-primary">
+                    Stars: {repo.stargazers_count}
+                  </li>
+                  <li className="badge badge-dark">
+                    Watchers: {repo.watchers_count}
+                  </li>
+                  <li className="badge badge-light">
+                    Forks: {repo.forks_count}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          })
+      }
+    </div>
+  );
+};
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired,
+  getGithubRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return {
+    repos: state.profile.repos
+  }
+}
+
+export default connect(mapStateToProps, {getGithubRepos})(ProfileGithub);
